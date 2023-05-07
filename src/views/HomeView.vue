a1a4b4901bf6ec596d35c882f953a5fa07930b0c
Johannes Borchard <johannesborchard@gmail.com>
2023-05-07 14:00:59 +0200

    Fertig

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -41,9 +41,16 @@ const events = ref([
 </script>
 
 <template>
-  <template>
-    <div class="home">
-      <EventCard v-for="event in events" :key="event.id" :event="event" />
-    </div>
-  </template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
 </template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
